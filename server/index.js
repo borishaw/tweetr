6b7fa5009add48b8505640ed2f1821923e4b6d7a
Boris Xiao <borishaw@gmail.com>
2017-03-25 18:55:18 +0000

    port change

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 // Basic express setup:
-const PORT          = 8080;
+require('dotenv').config();
+const PORT          = process.env.PORT;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,7 +19,7 @@ MongoClient.connect(MONGODB_URI, (err, db)=>{
 
   app.use("/tweets", tweetsRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT || 5000, () => {
     console.log("Tweeter app listening on port " + PORT);
   });
 
